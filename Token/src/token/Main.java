c56ab26b188aca7efc697ec13a52603a04eb5a66
Camylla Dias <dias_camylla@hotmail.com>
2022-09-14 20:22:20 -0300

    Update Main.java

--- Token/src/token/Main.java
+++ Token/src/token/Main.java
@@ -4,14 +4,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("entrada: ");
-        String texto = sc.nextLine();
-        Lexer lexer = new Lexer(texto);
-        Token token = lexer.getNextToken();
         
-        while(token.tipo != "EOF"){
-            System.out.println(token);
-            token = lexer.getNextToken();
+        try{
+            while(true){
+                System.out.println("Lexer: ");
+                String texto = sc.nextLine();
+                Lexer lexer = new Lexer(texto);
+                Token token = lexer.getNextToken();
+                
+                while(!token.getTipo().equals("EOF")){
+                    System.out.println(token);
+                    token = lexer.getNextToken();
+                }
+            }
+        }catch(RuntimeException re){
+            System.out.println("Error: " + re.getMessage());
+            sc.close();
         }
     }
 }
