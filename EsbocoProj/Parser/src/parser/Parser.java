2883f39aa85dccc9cd3b5f15f1296ddd47e06f4c
PatriciaMed <99925809+PatriciaMed@users.noreply.github.com>
2022-10-25 00:56:16 -0300

    Update Parser.java

--- EsbocoProj/Parser/src/parser/Parser.java
+++ EsbocoProj/Parser/src/parser/Parser.java
@@ -3,82 +3,84 @@ package parser;
 import java.util.ArrayList;
 
 public class Parser {
-    private ArrayList<Token> tokens;
-    private Token atual;
+  private ArrayList<Token> tokens;
+  private Token atual;
     
-    public Token getnextToken(){
+  public Token getnextToken(){
         Token t = tokens.get(0);
         tokens.remove(0);
         
         return t;
     }
     
-    private void erro(String regra) {
+  private void erro(String regra) {
         System.out.println("Regra: " + regra);
         System.out.println("token inválido: " + atual.getValor());
         System.exit(0);
     }
     
-    public void function(){
-        if (atual.getValor().equals("float")){
-            atual = getnextToken();
-            if (atual.getTipo().equals("id")){
-                atual = getnextToken();    
-                lparen();
-                if (atual.getTipo().equals("args")){
-                    atual = getnextToken();    
-                    rparen();
-                    if (atual.getValor().equals("{")){
-                        atual = getnextToken();  
-                        if (atual.getTipo().equals("comando")){
-                            atual = getnextToken();    
-                            if (atual.getValor().equals("}")){
-                                atual = getnextToken();    
-
-                            }else{
-                                erro("'}' esperado");
-                            }
-                        }else{
-                            erro("tipo Comando esperado");
-                        }
-
-                    }else{
-                        erro("'{' esperado");
-                    }
-                }else{
-                    erro("tipo args esperado");
-                }
-            }else{
-                erro("tipo Id esperado");
-            }
-        }else{
-            erro("'float' esperado");
-        }
-        
+  public void function(){
+    if (atual.getValor().equals("float")){
+      atual = getnextToken();
+    }else{
+      erro("'float' esperado");
+    }    
+  }
+  public void lparen(){
+    if(atual.getValor().equals("(")){
+      atual = getnextToken();
+    }else{
+      erro("'(' esperado");
     }
-    public void lparen(){
-        if(atual.getValor().equals("(")){
-            atual = getnextToken();
-        }else{
-            erro("'(' esperado");
-        }
+  }
+  public void rparen(){
+    if(atual.getValor().equals(")")){
+      atual = getnextToken();
+    }else{
+      erro("')' esperado");
     }
-    public void rparen(){
-        if(atual.getValor().equals(")")){
-            atual = getnextToken();
-        }else{
-            erro("')' esperado");
-        }
+  }
+  public void lchave(){
+    if (atual.getValor().equals("{")){
+      atual = getnextToken();    
+    }else{
+      erro("'{' esperado");
     }
-    
-    
-    public void fator(){
-        if(atual.getTipo().equals("id") ||atual.getTipo().equals("id")){
-           atual = getnextToken();
-        }else{
-            erro("Num ou Id esperado");
-        }
+  } 
+  public void rchave(){
+      if (atual.getValor().equals("}")){
+        atual = getnextToken();    
+      }else{
+        erro("'}' esperado");
+      }
     }
+  public void comando(){
+    if (atual.getTipo().equals("comando")){
+      atual = getnextToken();    
+        }else{
+          erro("tipo Comando esperado");
+      }
+  }
+  public void arguments(){
+    if (atual.getTipo().equals("args")){
+      atual = getnextToken();
+    }else{
+      erro("tipo args esperado");
+  }}
+  public void fator(){
+    if(atual.getTipo().equals("id") ||atual.getTipo().equals("id")){
+      atual = getnextToken();
+    }else{
+      erro("Num ou Id esperado");
+    }
+  }
+  public void id(){
+    if (atual.getTipo().equals("id")){
+      atual = getnextToken();
+    }else{
+      erro("tipo Id esperado");
+    }
+  }
     public Parser(ArrayList<Token> tokens){
         this.tokens = tokens;
         System.out.println(tokens.size());
