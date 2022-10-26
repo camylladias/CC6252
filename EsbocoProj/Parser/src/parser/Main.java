e45db18233b07cacbcbf29b6feabf5baf54f2e92
camylladias <dias_camylla@hotmail.com>
2022-10-25 23:31:17 -0300

    update aula charles terça

--- EsbocoProj/Parser/src/parser/Main.java
+++ EsbocoProj/Parser/src/parser/Main.java
@@ -11,15 +11,16 @@ public class Main {
       ArrayList<Token> tokens = new ArrayList<>();
       tokens.add(new Token("reservado", "float", 1)); //1 == length
       tokens.add(new Token("id", "equacao", 1));
-      tokens.add(new Token("parenl", "(", 1));
-      tokens.add(new Token("type", "int", 1));
+      tokens.add(new Token("lparen", "(", 1));
+      tokens.add(new Token("reservado", "int", 1));
       tokens.add(new Token("id", "x", 1));
-      tokens.add(new Token("parenr", ")", 1));
-      tokens.add(new Token("chavel", "{", 1));
-      tokens.add(new Token("id", "return", 1));
+      tokens.add(new Token("rparen", ")", 1));
+      tokens.add(new Token("lchave", "{", 1));
+      tokens.add(new Token("reservado", "return", 1));
       tokens.add(new Token("id", "x", 1));
-      tokens.add(new Token("chaver", "}", 1));
+      tokens.add(new Token("rchave", "}", 1));
       
+      System.out.println(tokens);
       Parser p = new Parser(tokens);
   }
 }
--- EsbocoProj/Parser/src/parser/Parser.java
+++ EsbocoProj/Parser/src/parser/Parser.java
@@ -5,14 +5,23 @@ import java.util.ArrayList;
 public class Parser {
   private ArrayList<Token> tokens;
   private Token atual;
-    
+  int controle=0; 
   public Token getnextToken(){
+    //verificar antes de chamar o next token se a lista esta vazia na prpxima iteracao, se n tiver eu posso fazer a chamada de função.
+        if (tokens.isEmpty()){
+          System.out.println("Lista vazia");
+          System.exit(0);
+        }
         Token t = tokens.get(0);
+        System.out.println(tokens);
         tokens.remove(0);
-        
+        /* if (controle<=t.getLength()){
+          System.out.println("next token funcao "+t);
+          controle=controle+1;
+        } */
         return t;
     }
-    //teste
+    
   private void erro(String regra) {
         System.out.println("Regra: " + regra);
         System.out.println("token inválido: " + atual.getValor());
@@ -20,39 +29,45 @@ public class Parser {
     }
 
   public void termo(){
+    System.out.println("Entrei na regra termo");
     fator();
     if(atual.getValor().equals('*')||atual.getValor().equals('/')){
       fator();
     }
   }
   public void expre(){
+    System.out.println("Entrei na regra expr");
     termo();
     if(atual.getValor().equals('+')||atual.getValor().equals('-')){
       termo();
     }
   }
   public void tipo(){
+    System.out.println("Entrei na regra tipo");
     if (atual.getValor().equals("int")||atual.getValor().equals("double")||atual.getValor().equals("boolean")){
       atual = getnextToken();
     }else{
-      erro("'float' esperado");
+      erro("'tipo de dado inválido");
     }
   }
+
   public void function(){
+    System.out.println("Entrei na regra fun");
     if (atual.getValor().equals("float")){
       atual = getnextToken();
       id();
       lparen();
-      arguments();
+      arguments(); 
       rparen();
       lchave();
       comando();
-      rchave();
+      rchave(); 
     }else{
       erro("'float' esperado");
     }    
   }
   public void lparen(){
+    System.out.println("Entrei na regra lparent");
     if(atual.getValor().equals("(")){
       atual = getnextToken();
     }else{
@@ -60,6 +75,8 @@ public class Parser {
     }
   }
   public void rparen(){
+    System.out.println("Entrei na regra rparent");
+    System.out.println("testeee  "+atual+atual.getValor()+atual.getTipo());
     if(atual.getValor().equals(")")){
       atual = getnextToken();
     }else{
@@ -67,6 +84,7 @@ public class Parser {
     }
   }
   public void lchave(){
+    System.out.println("Entrei na regra lchave");
     if (atual.getValor().equals("{")){
       atual = getnextToken();    
     }else{
@@ -74,6 +92,7 @@ public class Parser {
     }
   } 
   public void rchave(){
+    System.out.println("Entrei na regra rchave");
       if (atual.getValor().equals("}")){
         atual = getnextToken();    
       }else{
@@ -81,6 +100,7 @@ public class Parser {
       }
     }
   public void comando(){
+    System.out.println("Entrei na regra comando");
     if (atual.getValor().equals("return")){
       atual = getnextToken();
       expre();
@@ -89,6 +109,7 @@ public class Parser {
       }
   }
   public void arguments(){
+    System.out.println("Entrei na regra args");
     tipo();
     if (atual.getTipo().equals("id")){
       atual = getnextToken();
@@ -97,7 +118,8 @@ public class Parser {
     }
   }
   public void fator(){
-    if(atual.getTipo().equals("id") ||atual.getTipo().equals("id")){
+    System.out.println("valor do fator "+atual);
+    if(atual.getTipo().equals("id") ||atual.getTipo().equals("num")){
       atual = getnextToken();
     }else{
       lparen();
@@ -106,6 +128,7 @@ public class Parser {
     }
   }
   public void id(){
+    System.out.println("Entrei na regra id");
     if (atual.getTipo().equals("id")){
       atual = getnextToken();
     }else{
@@ -118,18 +141,16 @@ public class Parser {
   }
   public void Analiza(){
     atual = getnextToken();
-    while (tokens.isEmpty()==false){
-      System.out.println(tokens.size());
-      if(atual.getValor().equals("float")){
-        function();
-      }
-      if(atual.getValor().equals("return")){
+    // while (tokens.isEmpty()==false){
+    System.out.println("Valor do atual "+atual);
+     function();
+      /* if(atual.getValor().equals("return")){
         comando();
       }
       else{
         expre();
-      }
-    }
+      } */
+    // }
     System.out.println("Programa rodado por completo.");
   }
 }
